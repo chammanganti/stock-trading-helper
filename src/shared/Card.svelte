03bebc178b8876423eded0aa7aa7ec956b6be3d3
Cham Manganti <chammanganti@gmail.com>
2021-02-17 20:38:01 +0800

    feat: add a shared card component

---
+++ src/shared/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let bg = "";
+  export let roundness = "";
+  export let shadow = "";
+</script>
+
+<div class="px-6 py-8 {bg} {roundness} {shadow}">
+  <slot />
+</div>

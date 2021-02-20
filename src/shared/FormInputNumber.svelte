1f6465662b1bd143280ff9df2c5378b7dddb9f72
Cham Manganti <chammanganti@gmail.com>
2021-02-20 23:06:33 +0800

    feat(shared/forminputnumber.svelte): add ungrouped prop

--- src/shared/FormInputNumber.svelte
+++ src/shared/FormInputNumber.svelte
@@ -1,26 +1,35 @@
 <script lang="ts">
+  import FormLabel from "./FormLabel.svelte";
+
   export let disabled = false;
   export let label = "Label";
   export let placeholder = "Placeholder";
+  export let ungrouped = false;
   export let value: number;
 </script>
 
-<div class="md:flex md:items-center mb-6">
-  <div class="md:w-1/3">
-    <label
-      class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-      for="inline-full-name"
-    >
-      {label}
-    </label>
+{#if ungrouped === false}
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <FormLabel>{label}</FormLabel>
+    </div>
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-600"
+        {disabled}
+        {placeholder}
+        type="number"
+        bind:value
+      />
+    </div>
   </div>
-  <div class="md:w-2/3">
-    <input
-      class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-600"
-      {disabled}
-      {placeholder}
-      type="number"
-      bind:value
-    />
-  </div>
-</div>
+{:else}
+  <FormLabel>{label}</FormLabel>
+  <input
+    class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-600"
+    {disabled}
+    {placeholder}
+    type="number"
+    bind:value
+  />
+{/if}
